80ad86c35b7aebcccced40b5a1bbfec8f908429c
Naveenganesh <88182380+naveenganesh124@users.noreply.github.com>
2025-12-23 10:31:08 +0530

    Product CRUD API creation (#5)
    * Configure product entity and connected DB and addded get api to product
    
    * post, update, delete api for product

--- BusinessManager.API/Controllers/ProductsController.cs
+++ BusinessManager.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using BusinessManager.Application.Interfaces;
+﻿using BusinessManager.Application.DTOs;
+using BusinessManager.Application.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,7 +9,7 @@ namespace BusinessManager.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private  IProductService _productService;
+        private IProductService _productService;
         public ProductsController(IProductService productService)
         {
             _productService = productService;
@@ -19,6 +20,33 @@ namespace BusinessManager.API.Controllers
         {
             var products = await _productService.GetAllProductsAsync();
             return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct([FromBody] CreateProductDto dto)
+        {
+            await _productService.AddProductAsync(dto);
+            return Ok("Product added successfully");
+        }
+
+        [HttpPost("update")]
+        public async Task<IActionResult> UpdateProduct([FromBody] UpdateProductDto dto)
+        {
+            var updated = await _productService.UpdateProductAsync(dto);
+            if (!updated)
+                return NotFound("Product not found");
+
+            return Ok("Product updated successfully");
+        }
+
+        [HttpPost("delete")]
+        public async Task<IActionResult> DeleteProduct([FromBody] DeleteProductDto dto)
+        {
+            var deleted = await _productService.DeleteProductAsync(dto);
+            if (!deleted)
+                return NotFound("Product not found");
+
+            return Ok("Product deleted successfully");
         }
     }
 }
